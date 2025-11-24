27c8c0a2d389f53ac97f433ebe5e9055cb0710df
p0ice <67363618+zxhiend@users.noreply.github.com>
2025-11-24 22:27:28 +0700

    Delete .next directory

--- .next/dev/server/app/api/account/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/account/route.js")
-R.c("server/chunks/edded_next_ea5642ff._.js")
-R.c("server/chunks/[root-of-the-server]__6d405578._.js")
-R.c("server/chunks/df932_Vercel_week15-2025__next-internal_server_app_api_account_route_actions_40227567.js")
-R.m("[project]/UNTAR/Kuliah/Semester 3/1. Front-end Programming/Vercel/week15-2025/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/UNTAR/Kuliah/Semester 3/1. Front-end Programming/Vercel/week15-2025/app/api/account/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/UNTAR/Kuliah/Semester 3/1. Front-end Programming/Vercel/week15-2025/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/UNTAR/Kuliah/Semester 3/1. Front-end Programming/Vercel/week15-2025/app/api/account/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/dev/server/chunks/ssr/df932_Vercel_week15-2025__next-internal_server_app_about_page_actions_d48a7e23.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/UNTAR/Kuliah/Semester 3/1. Front-end Programming/Vercel/week15-2025/.next-internal/server/app/about/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=df932_Vercel_week15-2025__next-internal_server_app_about_page_actions_d48a7e23.js.map
